25ae20279c635522e975dac486e565eee55389e8
amir <amirmemool12@gmail.com>
2024-11-16 14:41:57 -0500

    :hammer: chore: init and close database

--- src/bootstrap/bootstrap.go
+++ src/bootstrap/bootstrap.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"DB_Project/src/api/http"
+	"DB_Project/src/database"
 	"context"
 	"github.com/joho/godotenv"
 	"log"
@@ -21,9 +22,16 @@ func Init() {
 	// Initialize Env variable
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("ENV Service: Failed to  loading .env file.    timestamp: %s", time.Now().String())
+		log.Fatalf("ENV Service: Failed to  loading .env file. %v.    timestamp: %s", err, time.Now().String())
 	}
 	log.Printf("ENV Service: Env variable initial successfully.    timestamp: %s \n", time.Now().String())
+
+	// Initialize Database
+	err = database.Init()
+	if err != nil {
+		log.Fatalf("Database Service: Failed to Initialize: %v.    timestamp: %s", err, time.Now().String())
+	}
+	log.Printf("Database Service: Database  initial successfully.    timestamp: %s \n", time.Now().String())
 
 	//Initialize http server
 	go func() {
@@ -40,11 +48,17 @@ func Init() {
 	<-sc
 
 	log.Printf("Application shutting down....    timestamp: %s \n", time.Now().String())
+	err = database.Close()
+	if err != nil {
+		log.Fatalf("Databasde Service: Failed to close database. %v.    timestamp: %s \n", err, time.Now().String())
+	}
+	log.Printf("Databasde Service: database close sucessfully.    timestamp: %s \n", time.Now().String())
+
 	err = http.ShutdownServer()
 	if err != nil {
 		log.Fatalf("HTTP Service: Failed to shutdown server. %v.    timestamp: %s \n", err, time.Now().String())
 	}
-	log.Printf("HTTP Service: server shutdwon sucessfully. %v.    timestamp: %s \n", err, time.Now().String())
+	log.Printf("HTTP Service: server shutdwon sucessfully.    timestamp: %s \n", time.Now().String())
 
 	time.Sleep(1 * time.Second)
 }

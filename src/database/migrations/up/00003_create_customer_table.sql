c21c2410600d196552611be34f605b75cf49896c
AmirHossein <amirmemool12@gmail.com>
2024-12-11 21:54:21 +0330

    :hammer: chore: add unique constrain to profile_id to make it one to one

--- src/database/migrations/up/00003_create_customer_table.sql
+++ src/database/migrations/up/00003_create_customer_table.sql
@@ -8,6 +8,6 @@ END $$;
 CREATE TABLE IF NOT EXISTS customer(
        id SERIAL PRIMARY KEY,
        type customer_type NOT NULL,
-       profile_id INT NOT NULL,
+       profile_id INT UNIQUE NOT NULL,
        CONSTRAINT fk_profile FOREIGN KEY (profile_id) REFERENCES profile (id) ON DELETE CASCADE
 );

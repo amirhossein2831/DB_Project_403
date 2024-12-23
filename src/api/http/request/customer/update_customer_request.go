6a487973f3ab7f40551c5fa0c413df9ccf20a909
AmirHossein <amirmemool12@gmail.com>
2024-12-24 01:08:04 +0330

    :hammer: chore: create update customer request

---
+++ src/api/http/request/customer/update_customer_request.go
@@ -0,0 +1,13 @@
+package customer
+
+import "DB_Project/src/models"
+
+type UpdateCustomerRequest struct {
+	FirstName    *string              `json:"first_name" validate:"omitempty"`
+	LastName     *string              `json:"last_name" validate:"omitempty"`
+	BirthDate    *string              `json:"birth_date" validate:"omitempty,datetime=2006-01-02"`
+	Phone        *string              `json:"phone" validate:"omitempty"`
+	Email        *string              `json:"email" validate:"omitempty,email"`
+	Address      *string              `json:"address" validate:"omitempty"`
+	CustomerType *models.CustomerType `json:"customer_type" validate:"omitempty,oneof=individual legal_entity"`
+}

1d90b10d5d177109d2de968f90ddbf2160432ece
Amirhossein Motaghian <a.motaghian@esavis.ir>
2024-12-23 17:19:16 +0330

    :hammer: chore: add field to scanner with join dynamically

--- src/utils/struct.go
+++ src/utils/struct.go
@@ -17,11 +17,51 @@ func FillStructFromRows(row pgx.Rows, model interface{}) error {
 
 	// Prepare a slice to hold pointers to the struct fields
 	v = v.Elem()
-	args := make([]interface{}, v.NumField())
+	args := make([]interface{}, 0)
 
 	// Iterate over each field of the struct and create a pointer to the field
 	for i := 0; i < v.NumField(); i++ {
-		args[i] = v.Field(i).Addr().Interface()
+		if sqlTag := v.Type().Field(i).Tag.Get("sql"); sqlTag != "" {
+			args = append(args, v.Field(i).Addr().Interface())
+		}
+	}
+
+	// Scan the row into the struct using the arguments
+	if err := row.Scan(args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FillStructFromRowsWithJoin(row pgx.Rows, model interface{}) error {
+	// Ensure the model is a pointer to a struct
+	v := reflect.ValueOf(model)
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
+		return ShouldBePointerToStruct
+	}
+
+	// Prepare a slice to hold pointers to the struct fields
+	v = v.Elem()
+	args := make([]interface{}, 0)
+
+	// Iterate over each field of the struct and create a pointer to the field
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if field.Kind() == reflect.Ptr && field.Type().Elem().Kind() == reflect.Struct {
+			// If the field is a pointer to a struct, initialize it and add its fields to args
+			field.Set(reflect.New(field.Type().Elem()))
+			subFields := field.Elem()
+			for j := 0; j < subFields.NumField(); j++ {
+				if sqlTag := subFields.Type().Field(i).Tag.Get("sql"); sqlTag != "" {
+					args = append(args, subFields.Field(j).Addr().Interface())
+				}
+			}
+		} else {
+			if sqlTag := v.Type().Field(i).Tag.Get("sql"); sqlTag != "" {
+				args = append(args, v.Field(i).Addr().Interface())
+			}
+		}
 	}
 
 	// Scan the row into the struct using the arguments
@@ -41,11 +81,51 @@ func FillStructFromRow(row pgx.Row, model interface{}) error {
 
 	// Prepare a slice to hold pointers to the struct fields
 	v = v.Elem()
-	args := make([]interface{}, v.NumField())
+	args := make([]interface{}, 0)
 
 	// Iterate over each field of the struct and create a pointer to the field
 	for i := 0; i < v.NumField(); i++ {
-		args[i] = v.Field(i).Addr().Interface()
+		if sqlTag := v.Type().Field(i).Tag.Get("sql"); sqlTag != "" {
+			args = append(args, v.Field(i).Addr().Interface())
+		}
+	}
+
+	// Scan the row into the struct using the arguments
+	if err := row.Scan(args...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FillStructFromRowWithJoin(row pgx.Row, model interface{}) error {
+	// Ensure the model is a pointer to a struct
+	v := reflect.ValueOf(model)
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
+		return ShouldBePointerToStruct
+	}
+
+	// Prepare a slice to hold pointers to the struct fields
+	v = v.Elem()
+	args := make([]interface{}, 0)
+
+	// Iterate over each field of the struct and create a pointer to the field
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		if field.Kind() == reflect.Ptr && field.Type().Elem().Kind() == reflect.Struct {
+			// If the field is a pointer to a struct, initialize it and add its fields to args
+			field.Set(reflect.New(field.Type().Elem()))
+			subFields := field.Elem()
+			for j := 0; j < subFields.NumField(); j++ {
+				if sqlTag := subFields.Type().Field(i).Tag.Get("sql"); sqlTag != "" {
+					args = append(args, subFields.Field(j).Addr().Interface())
+				}
+			}
+		} else {
+			if sqlTag := v.Type().Field(i).Tag.Get("sql"); sqlTag != "" {
+				args = append(args, v.Field(i).Addr().Interface())
+			}
+		}
 	}
 
 	// Scan the row into the struct using the arguments

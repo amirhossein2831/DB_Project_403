8253d409a2ed67e6730d062367d9df3c1224da80
AmirHossein <amirmemool12@gmail.com>
2024-12-24 22:52:51 +0330

    :hammer: chore: create employee controller,service,repository

---
+++ src/api/http/controller/employee_controller.go
@@ -0,0 +1,90 @@
+package controller
+
+import (
+	"DB_Project/src/api/http/request/employee"
+	"DB_Project/src/pkg/validation"
+	"DB_Project/src/services"
+	"errors"
+	"github.com/gofiber/fiber/v3"
+	"github.com/jackc/pgx/v4"
+)
+
+var EmployeeNotFound = errors.New("employee not found")
+
+type EmployeeController struct {
+	Service *services.EmployeeService
+}
+
+func NewEmployeeController() *EmployeeController {
+	return &EmployeeController{
+		Service: services.NewEmployeeService(),
+	}
+}
+
+func (controller *EmployeeController) List(c fiber.Ctx) error {
+	employees, err := controller.Service.GetEmployees()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"employees": employees,
+	})
+}
+
+func (controller *EmployeeController) Get(c fiber.Ctx) error {
+	id := c.Params("id")
+
+	res, err := controller.Service.GetEmployee(id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return c.Status(fiber.StatusNotFound).SendString(EmployeeNotFound.Error())
+		}
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(res)
+}
+
+func (controller *EmployeeController) Create(c fiber.Ctx) error {
+	req := new(employee.CreateEmployeeRequest)
+
+	if err := c.Bind().Body(req); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(validation.ValidateStruct(req))
+	}
+
+	err := controller.Service.CreateEmployee(req)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusCreated).Send([]byte{})
+}
+
+func (controller *EmployeeController) Update(c fiber.Ctx) error {
+	id := c.Params("id")
+
+	req := new(employee.UpdateEmployeeRequest)
+	if err := c.Bind().Body(req); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(validation.ValidateStruct(req))
+	}
+
+	err := controller.Service.UpdateEmployee(req, id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusCreated).Send([]byte{})
+}
+
+func (controller *EmployeeController) Delete(c fiber.Ctx) error {
+	id := c.Params("id")
+	err := controller.Service.DeleteEmployee(id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return c.Status(fiber.StatusNotFound).SendString(EmployeeNotFound.Error())
+		}
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	return c.Status(fiber.StatusNoContent).Send([]byte{})
+}
---
+++ src/services/employee_service.go
@@ -0,0 +1,109 @@
+package services
+
+import (
+	customer "DB_Project/src/api/http/request/employee"
+	"DB_Project/src/models"
+	"DB_Project/src/repositories"
+	"time"
+)
+
+type EmployeeService struct {
+	Repository        *repositories.EmployeeRepository
+	ProfileRepository *repositories.ProfileRepository
+}
+
+func NewEmployeeService() *EmployeeService {
+	return &EmployeeService{
+		Repository:        repositories.NewEmployeeRepository(),
+		ProfileRepository: repositories.NewProfileRepository(),
+	}
+}
+func (service *EmployeeService) GetEmployees() ([]*models.Employee, error) {
+	return service.Repository.List()
+}
+func (service *EmployeeService) GetEmployee(id string) (*models.Employee, error) {
+	return service.Repository.Get(id)
+}
+func (service *EmployeeService) CreateEmployee(req *customer.CreateEmployeeRequest) error {
+	// cast birth_date to date
+	birthDate, _ := time.Parse("2006-01-02", req.BirthDate)
+
+	// employee
+	employee := &models.Employee{
+		Position: req.Position,
+	}
+
+	// profile
+	profile := &models.Profile{
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		BirthDate: birthDate,
+		Phone:     req.Phone,
+		Email:     req.Email,
+		Address:   req.Address,
+	}
+
+	return service.Repository.Create(employee, profile)
+}
+
+// TODO: use one query to update several field
+func (service *EmployeeService) UpdateEmployee(req *customer.UpdateEmployeeRequest, id string) error {
+	if req.FirstName != nil && *req.FirstName != "" {
+		err := service.ProfileRepository.UpdateField("first_name", id, *req.FirstName)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.LastName != nil && *req.LastName != "" {
+		err := service.ProfileRepository.UpdateField("last_name", id, *req.LastName)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.BirthDate != nil && *req.BirthDate != "" {
+		err := service.ProfileRepository.UpdateField("birth_date", id, *req.BirthDate)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.Phone != nil && *req.Phone != "" {
+		err := service.ProfileRepository.UpdateField("phone", id, *req.Phone)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.Email != nil && *req.Email != "" {
+		err := service.ProfileRepository.UpdateField("email", id, *req.Email)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.Address != nil && *req.Address != "" {
+		err := service.ProfileRepository.UpdateField("address", id, *req.Address)
+		if err != nil {
+			return err
+		}
+	}
+
+	if req.Position != nil && *req.Position != "" {
+		err := service.Repository.UpdateField("position", id, *req.Position)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+func (service *EmployeeService) DeleteEmployee(id string) error {
+	_, err := service.Repository.Get(id)
+	if err != nil {
+		return err
+	}
+
+	return service.Repository.Delete(id)
+}

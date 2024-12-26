5505639b81be05e73fe755c66e94f01a8b6261c1
AmirHossein <amirmemool12@gmail.com>
2024-12-26 14:19:34 +0330

    :hammer: chore: change transaction request

--- src/api/http/request/transaction/create_transaction_request.go
+++ src/api/http/request/transaction/create_transaction_request.go
@@ -4,5 +4,5 @@ type CreateTransactionRequest struct {
 	Type                 string  `json:"type" validate:"required,oneof=deposit withdrawal transfer"`
 	Amount               float64 `json:"amount" validate:"required,gt=0"`
 	SourceAccountId      int     `json:"source_account_id" validate:"required,gt=0"`
-	DestinationAccountId int     `json:"destination_account_id" validate:"omitempty,gt=0"`
+	DestinationAccountId *int    `json:"destination_account_id" validate:"omitempty,gt=0"`
 }

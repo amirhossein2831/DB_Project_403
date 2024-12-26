3e5712caa7f2e02596430fc69c6e6cb4b5db21d0
AmirHossein <amirmemool12@gmail.com>
2024-12-26 14:43:46 +0330

    :hammer: chore: create loan repository,service and controller

---
+++ src/api/http/controller/loan_controller.go
@@ -0,0 +1,106 @@
+package controller
+
+import (
+	"DB_Project/src/api/http/request/loan"
+	"DB_Project/src/pkg/validation"
+	"DB_Project/src/services"
+	"DB_Project/src/utils"
+	"errors"
+
+	"github.com/gofiber/fiber/v3"
+	"github.com/jackc/pgx/v4"
+)
+
+var LoanNotFound = errors.New("loan not found")
+var LoanFieldShouldBeUnique = errors.New("loan field should be unique: ")
+var LoanIdNotSet = errors.New("loan id should be set")
+
+type LoanController struct {
+	Service *services.LoanService
+}
+
+func NewLoanController() *LoanController {
+	return &LoanController{
+		Service: services.NewLoanService(),
+	}
+}
+
+func (controller *LoanController) List(c fiber.Ctx) error {
+	loans, err := controller.Service.GetLoans()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"loans": loans,
+	})
+}
+
+func (controller *LoanController) Get(c fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(fiber.StatusBadRequest).SendString(LoanIdNotSet.Error())
+	}
+
+	res, err := controller.Service.GetLoan(id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return c.Status(fiber.StatusNotFound).SendString(LoanNotFound.Error())
+		}
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(res)
+}
+
+func (controller *LoanController) Create(c fiber.Ctx) error {
+	req := new(loan.CreateLoanRequest)
+
+	if err := c.Bind().Body(req); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(validation.ValidateStruct(req))
+	}
+
+	err := controller.Service.CreateLoan(req)
+	if err != nil {
+		if utils.IsErrorCode(err, "23505") {
+			return c.Status(fiber.StatusConflict).SendString(LoanFieldShouldBeUnique.Error() + utils.GetErrorConstraintName(err))
+		}
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	return c.Status(fiber.StatusCreated).Send([]byte{})
+}
+
+func (controller *LoanController) Update(c fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(fiber.StatusBadRequest).SendString(LoanIdNotSet.Error())
+	}
+
+	req := new(loan.UpdateLoanRequest)
+	if err := c.Bind().Body(req); err != nil {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(validation.ValidateStruct(req))
+	}
+
+	err := controller.Service.UpdateLoan(req, id)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).Send([]byte{})
+}
+
+func (controller *LoanController) Delete(c fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(fiber.StatusBadRequest).SendString(LoanIdNotSet.Error())
+	}
+
+	err := controller.Service.DeleteLoan(id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return c.Status(fiber.StatusNotFound).SendString(LoanNotFound.Error())
+		}
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	return c.Status(fiber.StatusNoContent).Send([]byte{})
+}
---
+++ src/repositories/loan_repository.go
@@ -0,0 +1,67 @@
+package repositories
+
+import (
+	"DB_Project/src/database/connection/pgx"
+	"DB_Project/src/models"
+	"DB_Project/src/utils"
+	"context"
+	"fmt"
+)
+
+type LoanRepository struct {
+}
+
+func NewLoanRepository() *LoanRepository {
+	return &LoanRepository{}
+}
+
+func (repository *LoanRepository) List() ([]*models.Loan, error) {
+	var loans []*models.Loan
+	rows, err := pgx.GetInstance().Query(context.Background(), "SELECT * FROM loan l INNER JOIN customer c ON l.customer_id = c.id")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var loan models.Loan
+		err = utils.FillStructFromRowsWithJoin(rows, &loan)
+		if err != nil {
+			return nil, err
+		}
+		loans = append(loans, &loan)
+	}
+
+	return loans, rows.Err()
+}
+
+func (repository *LoanRepository) Get(id string) (*models.Loan, error) {
+	var loan models.Loan
+	row := pgx.GetInstance().QueryRow(context.Background(), "SELECT * FROM loan l LEFT JOIN customer c ON l.customer_id = c.id WHERE l.id = $1", id)
+	err := utils.FillStructFromRowWithJoin(row, &loan)
+	if err != nil {
+		return nil, err
+	}
+
+	return &loan, nil
+}
+
+func (repository *LoanRepository) Create(loan *models.Loan) error {
+	// Insert loan record
+	_, err := pgx.GetInstance().Exec(context.Background(),
+		`INSERT INTO loan (customer_id, type, status, amount, interest_rate, repayment_period, finished_at) 
+		VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+		loan.CustomerID, loan.Type, loan.Status, loan.Amount, loan.InterestRate, loan.RepaymentPeriod, loan.FinishedAt,
+	)
+	return err
+}
+
+func (repository *LoanRepository) UpdateField(name, id string, value interface{}) error {
+	_, err := pgx.GetInstance().Exec(context.Background(), fmt.Sprintf("UPDATE loan SET %s = $1 WHERE id = $2", name), value, id)
+	return err
+}
+
+func (repository *LoanRepository) Delete(id string) error {
+	_, err := pgx.GetInstance().Exec(context.Background(), "DELETE FROM loan WHERE id=$1", id)
+	return err
+}

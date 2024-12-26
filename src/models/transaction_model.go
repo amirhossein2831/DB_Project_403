3512eb0f2572f9e8f6ef8dffe108e18d1bbb629b
AmirHossein <amirmemool12@gmail.com>
2024-12-26 14:16:16 +0330

    :hammer: chore: update transaction model

--- src/models/transaction_model.go
+++ src/models/transaction_model.go
@@ -6,9 +6,9 @@ type Transaction struct {
 	ID                   int        `json:"id" sql:"id"`
 	Type                 string     `json:"account_id" sql:"account_id"`
 	Amount               float64    `json:"amount" sql:"amount"`
-	SourceAccountId      string     `json:"description" sql:"description"`
-	DestinationAccountId string     `json:"type" sql:"type"`
+	SourceAccountId      int        `json:"description" sql:"source_account_id"`
+	DestinationAccountId *int       `json:"type" sql:"destination_account_id"`
 	CreatedAt            *time.Time `json:"created_at" sql:"created_at"`
-	SourceAccount        *Account   `json:"source_account" sql:"source_account"`
-	DestinationAccount   *Account   `json:"destination_account" sql:"destination_account"`
+	SourceAccount        *Account   `json:"source_account" sql:""`
+	DestinationAccount   *Account   `json:"destination_account" sql:"-"`
 }

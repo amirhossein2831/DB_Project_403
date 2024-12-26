b3546abf45ca757e089db20e5085d875cb52609d
AmirHossein <amirmemool12@gmail.com>
2024-12-26 14:57:42 +0330

    :hammer: chore: update loan seeder

--- src/database/seed/loan_seeder.go
+++ src/database/seed/loan_seeder.go
@@ -11,10 +11,10 @@ func LoanSeeder() {
 
 	query := `
 	INSERT INTO loan (customer_id, type, status, amount, interest_rate, repayment_period, created_at, finished_at)VALUES
-		(1, 'personal', 'approved', 2000.00, 5.5, 12, '2024-01-10 10:00:00', NULL),
-		(2, 'mortgage', 'pending', 100000.00, 3.0, 240, '2024-02-15 10:00:00', NULL),
+		(1, 'personal', 'approved', 2000.00, 5.5, 12, '2024-01-10 10:00:00', '2024-02-15 10:00:00'),
+		(2, 'mortgage', 'pending', 100000.00, 3.0, 240, '2024-02-15 10:00:00', '2024-02-15 10:00:00'),
 		(3, 'personal', 'approved', 15000.00, 4.2, 60, '2024-03-20 10:00:00', '2024-09-20 10:00:00'),
-		(4, 'business', 'pending', 50000.00, 6.0, 36, '2024-04-25 10:00:00', NULL),
+		(4, 'business', 'pending', 50000.00, 6.0, 36, '2024-04-25 10:00:00', '2024-02-15 10:00:00'),
 		(5, 'personal', 'approved', 3000.00, 5.0, 12, '2024-05-30 10:00:00', '2024-11-30 10:00:00');
 	`
 
